711f7278eff135f803f28dd8446e2dedda15cf11
Praneeth <praneethg807@gmail.com>
2025-03-27 19:21:44 +0530

    Your commit message

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true, // Required for App Router
+    },
+    reactStrictMode: true, // Recommended for Next.js
+  };
+  
+  export default nextConfig;
+  
